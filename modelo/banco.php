6a94f35447e3b61aace529e39dd4e55e328bbce3
Angela-0204 <angelarojas0204@gmail.com>
2025-05-03 15:32:06 -0400

    banco con trait

--- modelo/banco.php
+++ modelo/banco.php
@@ -8,18 +8,33 @@ class Banco extends Conexion {
     private $conex;
     private $nombre_banco;
     private $cod_banco;
+    private $errores = [];
+
     public function __construct() {
         $this->conex = (new Conexion())->conectar();
-    }
+    } 
 
-  
-    public function setNombre($nombre_banco) {
+    public function setNombre($nombre_banco){
+        $resultado = $this->validarTexto($nombre_banco, 'nombre', 2, 50);
+        if ($resultado === true) {
+            $this->nombre_banco = $nombre_banco;
+        } else {
+            $this->errores['nombre_banco'] = $resultado;
+        }
         $this->nombre_banco = $nombre_banco;
     }
-
     public function getNombreBanco() {
         return $this->nombre_banco;
     }
+
+     // Chequear si hay errores
+     public function check() {
+        if (!empty($this->errores)) {
+            $mensajes = implode(" | ", $this->errores);
+            throw new Exception("Errores de validación: $mensajes");
+        }
+    }
+
 
     // REGISTRAR
     private function registrar() {
